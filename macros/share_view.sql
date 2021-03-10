80ed6e581282231475f8d859329a5b2b50e93293
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-03-10 00:06:13 +0000

    fix table vs. view typo

--- macros/share_view.sql
+++ macros/share_view.sql
@@ -3,7 +3,7 @@
 
     {% set sql %}
       GRANT USAGE ON SCHEMA {{ target.database }}.{{ view_schema }} TO SHARE {{ share_name }};
-      GRANT SELECT ON TABLE {{ target.database }}.{{ view_schema}}.{{ view_name }} TO SHARE {{ share_name }}
+      GRANT SELECT ON VIEW {{ target.database }}.{{ view_schema}}.{{ view_name }} TO SHARE {{ share_name }}
     {% endset %}
 
     {% set table = run_query(sql) %}
