96729adba16b98a370fa1668991c95f9a620558a
Matthew Winkler <matt.winkler@fishtownanalytics.com>
2021-04-23 21:07:13 +0000

    final updates for SF

--- models/demo_examples/materialization_incremental.sql
+++ models/demo_examples/materialization_incremental.sql
@@ -3,7 +3,7 @@
            incremental_strategy='merge',
            unique_key='customer_key',
            should_full_refresh=False,
-           on_schema_change='sync') 
+           on_schema_change='append') 
 }}
 
 with customers as (
